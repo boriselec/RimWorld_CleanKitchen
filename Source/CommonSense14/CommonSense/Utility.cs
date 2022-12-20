29c177a96e4671867c5d9858ca7e13e79e4e5166
boriselec <boris.puzikov@gmail.com>
2022-12-20 11:27:09 +0300

    clean kitchen: del unrelated

--- Source/CommonSense14/CommonSense/Utility.cs
+++ Source/CommonSense14/CommonSense/Utility.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using HarmonyLib;
 using RimWorld;
 using Verse;
 using Verse.AI;
-using Verse.Sound;
 using UnityEngine;
 
 namespace CommonSense
@@ -13,8 +11,6 @@ namespace CommonSense
     [StaticConstructorOnStartup]
     public static class Utility
     {
-        public static readonly Texture2D texUnloadThing = ContentFinder<Texture2D>.Get("UI/Icons/Unload_Thing");
-        public static readonly Texture2D texUnloadThingCancel = ContentFinder<Texture2D>.Get("UI/Icons/Unload_Thing_Cancel");
         public static readonly Texture2D texMoteClean = ContentFinder<Texture2D>.Get("Things/Mote/Clean");
 
         private static WorkGiverDef cleanFilth = null;
@@ -171,151 +167,6 @@ namespace CommonSense
                     }
                 }
             }
-        }
-
-        public static void OptimizePath(List<ThingCount> q, Thing Starter = null)
-        {
-            if (q.Count > 0)
-            {
-                //int x;// = 0;
-                //int idx = 0;
-                //int n;// = 0;
-                ThingCount out_of_all_things_they_didnt_add_a_simple_swap;// = default(ThingCount);
-
-                if (Starter != null)
-                {
-                    var n = q[0].Thing.Position == null ? int.MaxValue : q[0].Thing.Position.DistanceToSquared(Starter.Position);
-                    int idx = 0;
-                    for (int i = 1; i < q.Count(); i++)
-                    {
-                        if (q[i].Thing.Position == null)
-                            continue;
-                        var x = q[i].Thing.Position.DistanceToSquared(Starter.Position);
-                        if (Math.Abs(x) < Math.Abs(n))
-                        {
-                            n = x;
-                            idx = i;
-                        }
-                    }
-                    if (idx != 0)
-                    {
-                        out_of_all_things_they_didnt_add_a_simple_swap = q[idx];
-                        q[idx] = q[0];
-                        q[0] = out_of_all_things_they_didnt_add_a_simple_swap;
-                    }
-                }
-
-                for (int i = 0; i < q.Count() - 1; i++)
-                {
-                    if (q[i + 1].Thing.Position == null)
-                        continue;
-
-                    var n = q[i].Thing.Position.DistanceToSquared(q[i + 1].Thing.Position);
-                    var idx = i + 1;
-                    for (int c = i + 2; c < q.Count(); c++)
-                    {
-                        if (q[c].Thing.Position == null)
-                            continue;
-
-                        var x = q[i].Thing.Position.DistanceToSquared(q[c].Thing.Position);
-                        if (Math.Abs(x) < Math.Abs(n))
-                        {
-                            n = x;
-                            idx = c;
-                        }
-                    }
-
-                    if (idx != i + 1)
-                    {
-                        out_of_all_things_they_didnt_add_a_simple_swap = q[idx];
-                        q[idx] = q[i + 1];
-                        q[i + 1] = out_of_all_things_they_didnt_add_a_simple_swap;
-                    }
-                }
-            }
-        }
-
-        public static bool ShouldHideFromWeather(this Pawn pawn)
-        {
-            if (!Settings.safe_wander
-                || pawn.Faction != Faction.OfPlayer
-                || !pawn.Map.IsPlayerHome
-                || pawn.mindState?.duty != null)
-                return false;
-            //
-            bool cares = pawn.needs?.mood != null;
-            if (cares)
-            {
-                if (JoyUtility.EnjoyableOutsideNow(pawn.Map))
-                    return false;
-            }
-            else if (!pawn.RaceProps.IsFlesh || pawn.Map.gameConditionManager.ActiveConditions.FirstOrDefault(x => x is GameCondition_ToxicFallout) == null)
-                return false;
-
-            return true;
-        }
-
-        public static bool IsBiocodedOrLinked(this Pawn pawn, Thing thing, bool? inventory = null)
-        {
-            return pawn.IsQuestLodger() && (inventory == true || !EquipmentUtility.QuestLodgerCanUnequip(thing, pawn));
-        }
-
-        public static bool IsLocked(this Pawn pawn, Thing thing)
-        {
-            Apparel apparel;
-            return (apparel = (thing as Apparel)) != null && pawn.apparel != null && pawn.apparel.IsLocked(apparel);
-        }
-
-        public static bool DrawThingRow(object tab, ref float y, ref float width, Thing thing, bool inventory)
-        {
-            if (!Settings.gui_manual_unload)
-                return true;
-
-            Pawn SelPawn = (Pawn)ITab_Pawn_Gear_Utility.LSelPawnForGear.GetValue(tab);
-            bool CanControl = (bool)ITab_Pawn_Gear_Utility.LCanControl.GetValue(tab);
-            Color hColor = new Color(1f, 0.8f, 0.8f, 1f);
-            //Log.Message($"CanControl={CanControl}, pc={SelPawn.IsColonistPlayerControlled}, spawned={SelPawn.Spawned}, pcHome={SelPawn.Map.IsPlayerHome}, bcoded={IsBiocodedOrLinked(SelPawn, thing, inventory)}, locked={IsLocked(SelPawn, thing)}");
-
-            Rect rect = new Rect(0f, y, width, 28f);
-            if ((thing is ThingWithComps)
-                && CanControl
-                && (inventory || SelPawn.IsColonistPlayerControlled || SelPawn.Spawned && !SelPawn.Map.IsPlayerHome)
-                && !IsBiocodedOrLinked(SelPawn, thing, inventory)
-                && !IsLocked(SelPawn, thing))
-            {
-                Rect rect2 = new Rect(rect.width - 24f, y, 24f, 24f);
-                CompUnloadChecker c = CompUnloadChecker.GetChecker(thing, false, true);
-                if (c.ShouldUnload)
-                {
-                    TooltipHandler.TipRegion(rect2, "UnloadThingCancel".Translate());
-
-                    var cl = GUI.color;
-                    if (Widgets.ButtonImage(rect2, texUnloadThingCancel, hColor))
-                    {
-                        SoundDefOf.Tick_High.PlayOneShotOnCamera(null);
-                        c.ShouldUnload = false;
-
-                        if (MassUtility.Capacity(SelPawn, null) < MassUtility.GearAndInventoryMass(SelPawn)
-                            && thing.stackCount * thing.GetStatValue(StatDefOf.Mass, true) > 0
-                            && !thing.def.destroyOnDrop)
-                        {
-                            ITab_Pawn_Gear_Utility.LInterfaceDrop.Invoke(tab, new object[] { thing });
-                        }
-                    }
-                    GUI.color = cl;
-                }
-                else
-                {
-                    TooltipHandler.TipRegion(rect2, "UnloadThing".Translate());
-                    if (Widgets.ButtonImage(rect2, texUnloadThing, Color.white))
-                    {
-                        SoundDefOf.Tick_High.PlayOneShotOnCamera(null);
-                        c.ShouldUnload = true;
-                    }
-                }
-                width -= 24f;
-            }
-            return true;
         }
     }
 }
